2e5e8a767c99a3f41213a23a5c535511ec3786f1
Nebenmor <nebenmor.anthony@gmail.com>
2024-07-02 11:58:06 +0100

    update sidebar with hamburger meenu

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/components/ui/Sidebar";
 import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
 
 export default function RootLayout({
   children,
@@ -14,12 +15,16 @@ export default function RootLayout({
 
         <div className="flex size-full flex-col">
           <div className="root-layout">
-            <Image src="icons/logo.svg" width={30} height={30} alt={'menu-icon'}
+            <Image src="icons/logo.svg" width={30} height={30} alt="menu-icon"
             />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
           </div>
+          {children}
         </div>
 
-        {children}
+        
     </main>
   );
 }
